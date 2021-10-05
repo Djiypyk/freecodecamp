2b4561a312168326d142dd129e7eed01fb822bc9
Mikalai Zarazaka <golmtz@gmail.com>
2021-10-05 20:09:27 +0300

    Add complite task and new folder

---
+++ LearnBasicJS/AlgoritmsJS/89.js
@@ -0,0 +1,15 @@
+var myMusic = [
+  {
+    artist: "Billy Joel",
+    title: "Piano Man",
+    release_year: 1973,
+    formats: ["CD", "8T", "LP"],
+    gold: true,
+  },
+  {
+    artist: "Noize MC",
+    title: "Last Album",
+    release_year: 2007,
+    formats: ["CD", "Free to play"],
+  },
+];
