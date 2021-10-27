8d3115c478ac5616411917f5139e0e629b632b5e
Mikalai Zarazaka <golmtz@gmail.com>
2021-10-27 11:21:22 +0300

    Add task

--- LearnBasicJS/Intermediate Algorithm Scripting/Task.js
+++ LearnBasicJS/Intermediate Algorithm Scripting/Task.js
@@ -95,3 +95,24 @@ function spinalCase(str) {
 }
 
 spinalCase("This Is Spinal Tap");
+
+//task 06
+
+/*Pig Latin
+Pig Latin is a way of altering English Words. The rules are as follows:
+
+- If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add ay to it.
+
+- If a word begins with a vowel, just add way at the end.
+
+Translate the provided string to Pig Latin. Input strings are guaranteed to be English words in all lowercase.*/
+
+function translatePigLatin(str) {
+  let constRegex = /^[^aeiou]+/;
+  let myConst = str.match(constRegex);
+  return myConst !== null
+    ? str.replace(constRegex, "").concat(myConst).concat("ay")
+    : str.concat("way");
+}
+
+translatePigLatin("consonant");
