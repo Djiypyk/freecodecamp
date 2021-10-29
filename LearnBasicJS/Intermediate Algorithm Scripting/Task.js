261a91c5797d7e37981cdd5efdc7c46f8e4fd510
Mikalai Zarazaka <golmtz@gmail.com>
2021-10-29 12:04:36 +0300

    add complete task

--- LearnBasicJS/Intermediate Algorithm Scripting/Task.js
+++ LearnBasicJS/Intermediate Algorithm Scripting/Task.js
@@ -238,7 +238,7 @@ function uniteUnique(arr1, arr2, arr3) {
 
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
 
-//task11
+//task 11
 
 /*Convert HTML Entities
 Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.*/
@@ -259,3 +259,29 @@ function convertHTML(str) {
 }
 
 convertHTML("Dolce & Gabbana");
+
+//task 12
+
+/*Sum All Odd Fibonacci Numbers
+Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
+
+The first two numbers in the Fibonacci sequence are 1 and 1. Every additional number in the sequence is the sum of the two previous numbers. The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
+
+For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.*/
+
+function sumFibs(num) {
+  let prevNumber = 0;
+  let currNumber = 1;
+  let result = 0;
+  while (currNumber <= num) {
+    if (currNumber % 2 !== 0) {
+      result += currNumber;
+    }
+    currNumber += prevNumber;
+    prevNumber = currNumber - prevNumber;
+  }
+
+  return result;
+}
+
+sumFibs(4);
