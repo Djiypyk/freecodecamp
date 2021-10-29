e718879fba471b2f3dc45ab96815c248a9c2e16c
Mikalai Zarazaka <golmtz@gmail.com>
2021-10-29 11:56:52 +0300

    add complete task

--- LearnBasicJS/Intermediate Algorithm Scripting/Task.js
+++ LearnBasicJS/Intermediate Algorithm Scripting/Task.js
@@ -209,3 +209,53 @@ function fearNotLetter(str) {
 }
 
 fearNotLetter("abce");
+
+// task 10
+
+/* Sorted Union
+Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+
+In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+
+The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+
+Check the assertion tests for examples.*/
+
+function uniteUnique(arr1, arr2, arr3) {
+  let finalArray = [];
+  for (let i = 0; i < arguments.length; i++) {
+    var arrayArguments = arguments[i];
+    for (let j = 0; j < arrayArguments.length; j++) {
+      let indexValue = arrayArguments[j];
+      if (finalArray.indexOf(indexValue) < 0) {
+        finalArray.push(indexValue);
+      }
+    }
+  }
+
+  return finalArray;
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+
+//task11
+
+/*Convert HTML Entities
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.*/
+
+function convertHTML(str) {
+  const htmlEntities = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&apos;",
+  };
+
+  return str
+    .split("")
+    .map((entity) => htmlEntities[entity] || entity)
+    .join("");
+}
+
+convertHTML("Dolce & Gabbana");
